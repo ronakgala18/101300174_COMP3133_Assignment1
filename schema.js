0d18f62af2d4cf12b09dddfaa3dcc08d870190ad
ronakgala18 <ronakgala18@gmail.com>
2023-02-19 17:37:00 -0500

    relovers and schemas added

---
+++ schema.js
@@ -0,0 +1,45 @@
+const { gql } = require('apollo-server-express');
+
+exports.typeDefs = gql `
+    type Employee {
+        id: ID!
+        firstname: String!
+        lastname: String!
+        email: String! 
+        gender: String!
+        salary: Float!
+        message: String
+        error: String
+    }
+    type User {
+        username: String!
+        email: String!
+        password: String!
+        message: String
+        error: String
+    }
+    type Query {
+        login(username: String!, password: String!): String
+        getEmployees: [Employee]
+        getEmployeeByID(id: ID!): Employee
+    }
+    type Mutation {
+        addEmployee(firstname: String!
+            lastname: String!
+            email: String!
+            gender: String!
+            salary: Float!): Employee
+        updateEmployee(id: String!
+            firstname: String!
+            lastname: String!
+            email: String!
+            gender: String!
+            salary: Float!): Employee
+        
+        deleteEmployee(id: String!): Employee
+        signup(username: String!, 
+            email: String!, 
+            password: String!): User
+                
+    }
+`
--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const TypeDefs = require('./schema')
-const Resolvers = require('./resolvers')
+const Resolvers = require('./resolver')
 const { ApolloServer } = require('apollo-server-express')
 const dotenv = require('dotenv');
-dotenv.config('./config.env');
+dotenv.config('./config.env'); 
 
+//mongoDB Atlas Connection String
+const db_url = process.env.DATABASE
 
-// DB Connection
-const DB = process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD);
-mongoose.set('strictQuery', true); // for suppressing the deprecation warning
-mongoose.connect(DB).then(() => {
-    console.log("Database successfully connected");
+mongoose.connect(db_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(success => {
+  console.log('Success Mongodb connection')
+}).catch(err => {
+  console.log('Error Mongodb connection')
 });
 
 //Define Apollo Server
@@ -32,10 +36,10 @@ async function startServer() {
 
   //Add Express app as middleware to Apollo Server
   server.applyMiddleware({ app });
-
+ const PORT = 3000;
   //Start listen 
-  app.listen({ port: process.env.PORT }, () =>
-    console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
+  app.listen(PORT, () =>
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
   );
 }
 
